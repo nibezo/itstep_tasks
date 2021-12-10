5a1093701056085dbcd5c16ffa829704d28d9543
nibezo <nibezo.cs@gmail.com>
2021-12-10 22:09:23 +0300

    Homework 1: Delete the 2nd task

--- lesson_1.py
+++ lesson_1.py
@@ -3,17 +3,3 @@ C помощью команды print() построчно выведите на
 консоль вот такое изображение ракеты:
 """
 print('  ___\n .    .\n:______:\n|      |\n|      |\n|      |\n|      |')
-
-"""
-Задание 2:
-
-Напишите программу, в которой пользователь отдельно
-вводит свое имя, возраст и номер телефона, а после ввода
-данных они выводятся на экран.
-"""
-
-name = input("Enter name: ")
-age = int(input("Enter age: "))
-phone_number = int(input("Enter phone number: "))
-
-print(f"Your name: {name}, your age: {age}, your phone number: {phone_number}")
